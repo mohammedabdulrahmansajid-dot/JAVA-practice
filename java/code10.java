c4419d7e54f9fbc108bd073d27920b5d85208381
Mohammed Abdul Rahman Sajid <mohammedabdulrahmansajid@gmail.com>
2026-08-20 22:29:10 +0530

    Add files via upload

---
+++ java/code10.java
@@ -0,0 +1,8 @@
+public class code10 {
+    public static void main(String[] args) {
+    float a = 4.8f;
+    float b = a % 1.1f;
+    System.out.println(b);
+    System.out.println(56 == 89);
+    System.out.println(43> 23 && 34<90);
+    }}
