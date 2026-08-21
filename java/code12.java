bd1243447bc6f51070062648a051eebc9190e294
Mohammed Abdul Rahman Sajid <mohammedabdulrahmansajid@gmail.com>
2026-08-21 20:29:11 +0530

    Add files via upload

---
+++ java/code12.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class code12 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number which you think will be greater than mine");
+        int a = s.nextInt();
+        float b = 67.0f;
+        boolean c = 67 < a; 
+        System.out.println(c);  
+    }
+}
---
+++ java/code13.java
@@ -0,0 +1,9 @@
+public class code13 {
+    public static void main(String[] args){
+    int a = 2;
+    int s = 1;
+    int v = 4;
+    int u = 3;
+    System.out.println( (float)(v*v - u*u)/(2*a*s));
+    }
+}
