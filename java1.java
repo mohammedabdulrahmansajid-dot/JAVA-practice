05856969752d6a604dc48d76a814a50f558fb1f6
Mohammed Abdul Rahman Sajid <mohammedabdulrahmansajid@gmail.com>
2026-08-23 20:58:12 +0530

    Add files via upload

---
+++ java1.java
@@ -0,0 +1,9 @@
+public class java1{
+    public static void main(String[] args){
+        float a = 2.8f;
+        float b = 9.876f;
+        float c = a * b;
+        System.out.printf("The multiplication of %2.2f and %4.2f is %10.1f", a , b , c);
+
+    }
+}
