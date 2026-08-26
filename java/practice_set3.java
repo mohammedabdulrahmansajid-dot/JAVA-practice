97767fdb3a5d8990e7540ac8eb90d2cdfdc651c1
Mohammed Abdul Rahman Sajid <mohammedabdulrahmansajid@gmail.com>
2026-08-26 22:19:48 +0530

    Add files via upload

---
+++ java/practice_set3.java
@@ -0,0 +1,21 @@
+public class practice_set3 {
+    public static void main(String [] args){
+        String S = "Maw Maw";
+
+        //Problem 1
+        System.out.println(S.toLowerCase());
+
+        // Problem 2
+        System.out.println(S.replace("a" , "eo")); 
+        
+        // Problem 3
+        String f = "Dear <name>, Thanks a lot";
+        System.out.println(f.replace("<name>", "Abdul Rahman"));
+        
+        // Problem 5
+        System.out.println("Dear MARS,\n\t This Java course is nice\n Thanks");
+
+
+    }
+    
+}
