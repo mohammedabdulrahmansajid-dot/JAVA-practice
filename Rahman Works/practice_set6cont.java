8552cfca1f3caf0fb138341265ab11023b365d6c
Mohammed Abdul Rahman Sajid <mohammedabdulrahmansajid@gmail.com>
2026-09-01 21:32:00 +0530

    Add files via upload

--- Rahman Works/practice_set6cont.java
+++ Rahman Works/practice_set6cont.java
@@ -20,10 +20,29 @@ public class practice_set6cont{
     // }           
       
     // PROBLEM 5: 
-    int [] data = {98, 74, 656, 938, 03,};
-    for(int i = data.length-1; i>=0; i--){
-        System.out.println(data[i]);
-    }  
+    // int [] data = {98, 74, 656, 938, 03,};
+    // for(int i = data.length-1; i>=0; i--){
+    //     System.out.print(data[i]);
+    //     System.out.print(" ");
+        
+    // }  
+    // Other Important Method For reversing:
+    int[] arr = {12 , 87 , 9 , 1 , 34};
+    int l = arr.length;
+    int n = Math.floorDiv(l , 2);
+    int temp;
+    for(int i = 0; i<n; i++){
+        temp = arr[i];
+        arr[i] = arr[l-1-i];
+        arr[l-1-i] = temp;}
+        for(int j = 0; j<arr.length; j++){
+            System.out.print(arr[j]);
+            System.out.print(" ");}
+        
+
+
+    
+
 
 
 
